d738b278b07bf9f80baebb6581ad88e904ff7ed8
Mariusz Szleszynski <mariusz.szleszynski@outlook.com>
2020-10-07 12:25:11 +0200

    Init Accordion

---
+++ src/js/script.js
@@ -0,0 +1,180 @@
+/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+
+{
+  'use strict';
+
+  const select = {
+    templateOf: {
+      menuProduct: '#template-menu-product',
+    },
+    containerOf: {
+      menu: '#product-list',
+      cart: '#cart',
+    },
+    all: {
+      menuProducts: '#product-list > .product',
+      menuProductsActive: '#product-list > .product.active',
+      formInputs: 'input, select',
+    },
+    menuProduct: {
+      clickable: '.product__header',
+      form: '.product__order',
+      priceElem: '.product__total-price .price',
+      imageWrapper: '.product__images',
+      amountWidget: '.widget-amount',
+      cartButton: '[href="#add-to-cart"]',
+    },
+    widgets: {
+      amount: {
+        input: 'input[name="amount"]',
+        linkDecrease: 'a[href="#less"]',
+        linkIncrease: 'a[href="#more"]',
+      },
+    },
+  };
+
+  const classNames = {
+    menuProduct: {
+      wrapperActive: 'active',
+      imageVisible: 'active',
+    },
+  };
+
+  const settings = {
+    amountWidget: {
+      defaultValue: 1,
+      defaultMin: 1,
+      defaultMax: 9,
+    }
+  };
+
+  const templates = {
+    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+  };
+
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+
+      console.log('new Product:', thisProduct);
+    }
+
+    renderInMenu() {
+      const thisProduct = this;
+
+      //generate HTML based on template
+
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      console.log('generatedHTML:', generatedHTML);
+
+      //create element using unlits.createElementFromHTML
+
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      console.log('thisProduct:', thisProduct);
+
+
+      // find menu container   -> <div id="product-list" class="product-list container"></div>
+
+      const menuContainer = document.querySelector(select.containerOf.menu);   // ?????? do wyjaśnienia co właściwie szuka
+
+      console.log('menuContainer:', menuContainer);
+
+      // add element to menu
+
+      menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion() {
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+
+      const clickableTigger = select.menuProduct.clickable
+
+      /* START: click event listener to trigger */
+
+      clickableTigger.addEventListener('click', function () {
+
+        /* prevent default action for event */
+
+        preventDefault();
+
+        /* toggle active class on element of thisProduct */
+
+        thisProduct.element = select.menuProduct.clickable
+
+        /* find all active products */
+
+        const activeProducts = document.querySelectorAll('active');
+
+        /* START LOOP: for each active product */
+
+        for (let activeProduct of activeProducts) {
+
+          /* START: if the active product isn't the element of thisProduct */
+
+          if (activeProduct != thisProduct) {
+
+            /* remove class active for the active product */
+
+            activeProduct.classList.remove('active')
+
+            /* END: if the active product isn't the element of thisProduct */
+          }
+          /* END LOOP: for each active product */
+
+        }
+
+        /* END: click event listener to trigger */
+
+      });
+
+
+    }
+
+  }
+
+  const app = {
+
+    initMenu: function () {
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+
+      // const testProduct = new Product();
+      // console.log('testProduct:', testProduct);
+
+    },
+
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
+    init: function () {
+      const thisApp = this;
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
+    },
+  };
+
+
+
+  app.init();
+}
